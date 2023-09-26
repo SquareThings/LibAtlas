df3a0e3ca2bbe857ab070d8c7dff44485fa1a06b
ssompal1 <shravya_sompalli@brown.edu>
2023-09-26 12:18:53 -0400

    update map html to recent

--- my-app/src/pages/NYC.js
+++ my-app/src/pages/NYC.js
@@ -11,7 +11,7 @@ function NYC() {
             <h2 className="headerMapTextTwo">Ongoing Work</h2>
             <div className="mapTextContainer"><p className="mapText">
                 We are working to expand our analysis of state surveillance and policing of migrant massage and sex workers beyond arrests by examining building code and housing violations issued by the New York Department of Buildings and New York Department of Housing Preservation and Development (HPD). In announcing in April of 2021 that it would no longer prosecute people charged on counts of prostitution, the Manhattan DA’s office reflects some movement away from the explicit policing of sex workers. We seek to understand whether there is data to support that explicit policing is being replaced with more implicit exercises of state power that still have serious negative impacts on migrant massage and sex workers. We are currently in the process of examining the overlap between locations of violations and of massage parlors by scraping and cleaning location data from sources such as RubMaps and Google Reviews and matching this data with housing violations data published by NYDOB and NYHPD. </p></div>
-            <iframe class="embedMap" src="https://squarethings.github.io/NYCMap/" title="NYC Map of Prostitution and Massage Arrests"></iframe>
+            <iframe class="embedMap" src="https://ssompal1.github.io/LiberationAtlasMap/public/index" title="NYC Map of Prostitution and Massage Arrests"></iframe>
         </div >
     );
 }
